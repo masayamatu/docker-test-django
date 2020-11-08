80cb67e31cf213a63b81bfd54860da44f6b0c686
masayamatu <m.masa1104@outlook.jp>
2020-11-08 22:29:12 +0900

    R021108-2

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p7=hm@0r&96jgrof%=b39m$xs0tg9l2g^y5unv)sm__nv%ios)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
